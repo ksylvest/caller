f6859a69d07d6ea9715f55d45c660d4bc0e6615f
Kevin Sylvestre <kevin@ksylvest.com>
2013-01-21 20:41:31 -0800

    renaming

--- assets/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-//= require ./libraries/jquery
-//= require ./libraries/jquery.phono
-//= require ./libraries/underscore
-//= require ./libraries/backbone
-//= require_tree ./application
---
+++ assets/javascripts/application.js.coffee
@@ -0,0 +1,5 @@
+#= require ./libraries/jquery
+#= require ./libraries/jquery.phono
+#= require ./libraries/underscore
+#= require ./libraries/backbone
+#= require_tree ./application
